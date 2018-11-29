fdf7b6816b8c55f712171e2cecdce1d684ca98fa
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-29 13:08:05 -0600

    made a path for class earnings

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from app import views
 urlpatterns = [
     path('add/', views.Add.as_view(), name='add'),
     path('double/', views.Double.as_view(), name='double'),
-    path('multthree/', views.Mult_three.as_view(), name='multthree')
+    path('multthree/', views.Mult_three.as_view(), name='multthree'),
+    path('earnings/', views.Earnings.as_view(), name='earnings')
 ]
