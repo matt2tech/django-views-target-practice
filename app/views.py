d651593627a8a783e16b77ccc26dffcc122cd687
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-29 16:01:56 -0600

    Built class WalkOrDrive

--- app/views.py
+++ app/views.py
@@ -37,15 +37,16 @@ class Mult_three(View):
 class Earnings(View):
     def get(self, request):
         try:
-            num1 = max(float(request.GET.get('num1', '')), 0)
-            num2 = max(float(request.GET.get('num2', '')), 0)
-            num3 = max(float(request.GET.get('num3', '')), 0)
+            num1 = max(int(request.GET.get('num1', '')), 0)
+            num2 = max(int(request.GET.get('num2', '')), 0)
+            num3 = max(int(request.GET.get('num3', '')), 0)
         except ValueError:
             return render(request, 'app/earnings.html')
         else:
             return render(request, 'app/earnings.html', {'answer': (num1 * 15 + num2 * 12 + num3 * 9)})
 
 class Both(View):
+    # radio buttons were used. Only True and False radio buttons
     def get(self, request):
         try:
             bool1 = request.GET.get('bool1', '') == 'True'
@@ -54,3 +55,18 @@ class Both(View):
             return render(request, 'app/both.html')
         else:
             return render(request, 'app/both.html', {'answer': (bool1 == True and bool2 == True)})
+
+class WalkOrDrive(View):
+    # radio button is used for the bool
+    def get(self, request):
+        try:
+            num = float(request.GET.get('num', ''))
+            bool = request.GET.get('bool', '') == 'True'
+        except ValueError:
+            return render(request, 'app/walk-or-drive.html')
+        else:
+            if num <= 0.25 and bool is True:
+                return render(request, 'app/walk-or-drive.html', {'answer': 'walk'})
+            else:
+                return render(request, 'app/walk-or-drive.html', {'answer': 'drive'})
+
