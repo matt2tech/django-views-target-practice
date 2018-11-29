0e9cdf3a4843711a03650fff4d9e3f6bfb53c1f4
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-29 14:43:22 -0600

    Added a path for class both

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('add/', views.Add.as_view(), name='add'),
     path('double/', views.Double.as_view(), name='double'),
     path('multthree/', views.Mult_three.as_view(), name='multthree'),
-    path('earnings/', views.Earnings.as_view(), name='earnings')
+    path('earnings/', views.Earnings.as_view(), name='earnings'),
+    path('both/', views.Both.as_view(), name='both'),
 ]
