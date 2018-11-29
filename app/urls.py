f2e24299c35daabaaee2f7cbd07cc9cc7c15a9da
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-29 08:33:03 -0600

    Initial commit

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 
 urlpatterns = [
     path('add/', views.Add.as_view(), name='add'),
+    path('double/', views.Double.as_view(), name='double'),
+    path('multthree/', views.Mult_three.as_view(), name='multthree')
 ]
--- app/views.py
+++ app/views.py
@@ -14,3 +14,25 @@ class Add(View):
             return render(request, 'app/add.html', {'answer': answer})
         else:
             return render(request, 'app/add.html')
+
+class Double(View):
+    def get(self, request):
+        num = request.GET.get('num')
+
+        if num is not None:
+            answer = float(num) * 2
+            return render(request, 'app/double.html', {'answer': answer})
+        else:
+            return render(request, 'app/double.html')
+
+class Mult_three(View):
+    def get(self, request):
+        num1 = request.GET.get('num1')
+        num2 = request.GET.get('num2')
+        num3 = request.GET.get('num3')
+
+        if num1 is not None and num2 is not None and num3 is not None:
+            answer = float(num1) * float(num2) * float(num3)
+            return render(request, 'app/multthree.html', {'answer': answer})
+        else:
+            return render(request, 'app/multthree.html')
