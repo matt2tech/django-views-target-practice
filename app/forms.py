250ffbe0a4c810c4fd128a7f61514a830fe6772f
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-30 09:17:52 -0600

    Added form for class MultThree

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,8 @@ class AddForm(Form):
 
 class DoubleForm(Form):
     num = forms.FloatField()
+
+class MultThree(Form):
+    num1 = forms.FloatField()
+    num2 = forms.FloatField()
+    num3 = forms.FloatField()
