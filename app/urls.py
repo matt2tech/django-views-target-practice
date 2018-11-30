6b98f2b7bbe765c33689571f38b11c5d347782fc
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-30 09:17:58 -0600

    Small editing

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app import views
 urlpatterns = [
     path('add/', views.Add.as_view(), name='add'),
     path('double/', views.Double.as_view(), name='double'),
-    path('multthree/', views.Mult_three.as_view(), name='multthree'),
+    path('multthree/', views.MultThree.as_view(), name='multthree'),
     path('earnings/', views.Earnings.as_view(), name='earnings'),
     path('both/', views.Both.as_view(), name='both'),
     path('walk-or-drive/', views.WalkOrDrive.as_view(), name='walk-or-drive'),
